bc6f2ef69df524db125d97f6ce27b0512ae45fb5
debian <debian@debian>
2025-04-12 11:27:21 +0300

    all good mai trebuie modificat putin in remove treasure

--- treasure_hunt.c
+++ treasure_hunt.c
@@ -8,6 +8,8 @@
 #include <fcntl.h>
 #include <time.h>
 
+#define CHUNK 10
+
 typedef enum {
     ADD,
     LIST,
@@ -58,53 +60,82 @@ void add(const char *hunt_id, Treasure treasure) {
     char *logMessage=(char*)malloc(500*sizeof(char));
     logMessage[0]=0;
     DIR *mydir;
+    
     struct dirent *myfile;
+    char huntTreasure[200]="";
+    sprintf(huntTreasure,"Treasures%s",hunt_id);
     //struct stat mystat;
 
     mydir = opendir("proiect");
     chdir("proiect");
-    //myfile = readdir(mydir);
+    myfile = readdir(mydir);
+    if (mydir == NULL) {
+        perror("Eroare la deschiderea directorului proiect");
+        return;
+    }
     char flag=0;
+    
 
     while((myfile = readdir(mydir)) != NULL)
     {
+        if (strcmp(myfile->d_name, ".") == 0 || strcmp(myfile->d_name, "..") == 0)
+        continue;
         // stat(myfile->d_name, &mystat);    
-        // printf("%d",mystat.st_size);
-        // printf(" %s\n", myfile->d_name);
+        //printf("%d",mystat.st_size);
+        //printf(" %s\n", myfile->d_name);
         if(strcmp(myfile->d_name, hunt_id) == 0) {
             flag = 1;
             chdir(hunt_id);
-            int fd = open(treasure.id, O_RDWR | O_CREAT | O_TRUNC, 0644);
             
-            write(fd, &treasure, sizeof(Treasure));
-            close(fd);
-            
-            char aux[300] = "";
-            sprintf(aux, "Added %s to %s\n", treasure.id, hunt_id);
-            strcat(logMessage, aux);
-            createLog(hunt_id, logMessage);
-            free(logMessage);
-            chdir("..");
             break;
         }
         
     }
     
+    
+    
+    //int fd = open(huntTreasure, O_RDWR | O_CREAT | O_APPEND, 0644);
+    if(flag) {
+        //struct dirent *treasureFile;
+        
+        int fd = open(huntTreasure, O_WRONLY | O_CREAT | O_APPEND, 0644);
+        if (fd == -1) {
+            perror("Eroare la open() pentru treasures.dat");
+        } else {
+            printf("Scriu in %s\n", huntTreasure);
+            ssize_t w = write(fd, &treasure, sizeof(Treasure));
+            if (w == -1)
+                perror("Eroare la write()");
+            else
+                printf("Am scris %ld bytes in %s\n", w, huntTreasure);
+                        
+            close(fd);
+        }
+        
 
-    if(!flag) {
-        int result = mkdir(hunt_id, S_IRWXU | S_IRWXG | S_IRWXO);
+        char aux[300] = "";
+        sprintf(aux, "Added %s to %s\n", treasure.id, hunt_id);
+        strcat(logMessage, aux);
+        createLog(hunt_id, logMessage);
+        free(logMessage);
+        chdir("..");
+    }
+    
+
+    else {
+        int result = mkdir(hunt_id, 0777);
         chdir(hunt_id);
         
-        int fd = open(treasure.id, O_RDWR | O_CREAT | O_TRUNC, S_IRWXU | S_IRWXG | S_IRWXO);
-
-        write(fd, &treasure, sizeof(Treasure));
-        close(fd);
-        struct stat mystat;
-        if (stat(hunt_id, &mystat) == 0) {
-            printf("Size: %ld\n", mystat.st_size);
-            printf("Ultima modificare: %s", ctime(&mystat.st_mtime));
-            strcat(logMessage, ctime(&mystat.st_mtime));
+        int fd = open(huntTreasure, O_RDWR | O_CREAT | O_APPEND, 0664);
+        if (fd == -1) {
+            perror("Eroare la open() pentru treasures.dat");
+        } else {
+            printf("Scriu in %s\n", huntTreasure);
+            write(fd, &treasure, sizeof(Treasure));
+            close(fd);
         }
+
+        
 
         char aux[300] = "";
         sprintf(aux, "Added %s to %s\n", treasure.id, hunt_id);
@@ -119,24 +150,27 @@ void add(const char *hunt_id, Treasure treasure) {
         else {
             printf("Succes\n");
         }
+
+        
     }   
 
     
 
     closedir(mydir);
-    mydir = opendir("proiect");
+    //mydir = opendir("proiect");
 }
 
 void list(const char *hunt_id) {
     char *logMessage=(char*)malloc(500*sizeof(char));
     logMessage[0]=0;
     DIR *mydir;
+    
     struct dirent *myfile;
     //struct stat mystat;
 
     mydir = opendir("proiect");
     chdir("proiect");
-    myfile = readdir(mydir);
+    //myfile = readdir(mydir);
     char flag=0;
     while((myfile=readdir(mydir)) != NULL) {
         if(!strcmp(myfile->d_name, hunt_id)) {
@@ -151,14 +185,21 @@ void list(const char *hunt_id) {
             DIR *hunt_dir = opendir(hunt_id);
             chdir(hunt_id);
             while((myfile=readdir(hunt_dir)) != NULL) {
-                if (strcmp(myfile->d_name, ".") == 0 || strcmp(myfile->d_name, "..") == 0)
+                if (strcmp(myfile->d_name, ".") == 0 || strcmp(myfile->d_name, "..") == 0 || strstr(myfile->d_name, "Log")!=NULL)
                     continue;
-                Treasure treasure;
-                int fd = open(myfile->d_name, O_RDONLY);
+                else {
+                    Treasure treasure;
+                    int fd = open(myfile->d_name, O_RDONLY);
+                    while(read(fd, &treasure, sizeof(Treasure)) == sizeof(Treasure)) {
+                        printf("%15s %29s %f %f %29s %d\n",treasure.id, treasure.text, treasure.coordinates.latitudine, treasure.coordinates.longitudine, treasure.clue, treasure.val);
+                    }
+                    close(fd);
+                }
+                
                 //printf("Nume fisier: %s\n",myfile->d_name);
-                read(fd, &treasure, sizeof(Treasure));
-                printf("%9s %s %f %f %29s %d\n",treasure.id, treasure.text, treasure.coordinates.latitudine, treasure.coordinates.longitudine, treasure.clue, treasure.val);
-                close(fd);
+                //read(fd, &treasure, sizeof(Treasure));
+                
+                
             }
             char aux[300] = "";
             sprintf(aux, "Listing treasures in hunt %s\n", hunt_id);
@@ -179,12 +220,10 @@ void view(const char *treasure_id) {
     struct dirent *hunt_entry;
     char *logMessage=(char*)malloc(500*sizeof(char));
     logMessage[0]=0;
-
+    char found = 0;
     main_dir = opendir("proiect");
 
     chdir("proiect");
-
-    char found = 0;
 
     while ((hunt_entry = readdir(main_dir)) != NULL) {
         if (strcmp(hunt_entry->d_name, ".") == 0 || strcmp(hunt_entry->d_name, "..") == 0)
@@ -201,11 +240,11 @@ void view(const char *treasure_id) {
 
         struct dirent *file_entry;
         while ((file_entry = readdir(hunt_dir)) != NULL) {
-            if (strcmp(file_entry->d_name, ".") == 0 || strcmp(file_entry->d_name, "..") == 0)
+            if (strcmp(file_entry->d_name, ".") == 0 || strcmp(file_entry->d_name, "..") == 0 || strstr(file_entry->d_name, "Log")!=NULL)
                 continue;
 
-            if (!strcmp(file_entry->d_name, treasure_id)) {
-                found = 1;
+            else {
+                
                 Treasure treasure;
                 int fd = open(file_entry->d_name, O_RDONLY);
                 if (fd < 0) {
@@ -213,13 +252,19 @@ void view(const char *treasure_id) {
                     break;
                 }
 
-                read(fd, &treasure, sizeof(Treasure));
-                close(fd);
-
-                printf("Nume fisier: %s\n", file_entry->d_name);
-                printf("%9s %29s %f %f %29s %d\n", treasure.id, treasure.text,
+                while(read(fd, &treasure, sizeof(Treasure)) == sizeof(Treasure)) {
+                    if(!strcmp(treasure.id, treasure_id)) {
+                        found=1;
+                        printf("Nume fisier: %s\n", file_entry->d_name);
+                        printf("%9s %29s %f %f %29s %d\n", treasure.id, treasure.text,
                        treasure.coordinates.latitudine, treasure.coordinates.longitudine,
                        treasure.clue, treasure.val);
+                       break;
+                    }
+                }
+                close(fd);
+
+                
                 char aux[300] = "";
                 sprintf(aux, "Viewed %s from %s\n", treasure.id, hunt_entry->d_name);
                 strcat(logMessage, aux);
@@ -268,11 +313,42 @@ void remove_treasure(const char *treasure_id) {
 
         struct dirent *file_entry;
         while ((file_entry = readdir(hunt_dir)) != NULL) {
-            if (strcmp(file_entry->d_name, ".") == 0 || strcmp(file_entry->d_name, "..") == 0)
+            if (strcmp(file_entry->d_name, ".") == 0 || strcmp(file_entry->d_name, "..") == 0 || strstr(file_entry->d_name,"Log")!=NULL)
                 continue;
+            
+            else {
+                int fd = open(file_entry->d_name, O_RDONLY);
+                if (fd < 0) {
+                    perror("Eroare la deschiderea fisierului");
+                    break;
+                }
 
-            if (!strcmp(file_entry->d_name, treasure_id)) {
-                found = 1;
+                Treasure treasure;
+                while(read(fd, &treasure, sizeof(Treasure)) == sizeof(Treasure)) {
+                    if(!strcmp(treasure.id, treasure_id)) {
+                        found = 1;
+                        long offsetStart = SEEK_CUR - sizeof(Treasure);
+                        int crt_size=CHUNK;
+                        int index=0;
+                        Treasure *aux=(Treasure*)malloc(crt_size*sizeof(Treasure));
+                        while(read(fd, &aux[index], sizeof(Treasure)) == sizeof(Treasure)) {
+                            if(index>=crt_size) {
+                                crt_size+=CHUNK;
+                                aux=(Treasure*)realloc(aux,crt_size*sizeof(Treasure));
+                            }
+                        }
+                        lseek(fd, 0, offsetStart);
+                        for(int i=0;i<index;i++) {
+                            write(fd, &aux[i], sizeof(Treasure));
+                        }
+                        free(aux);
+                        break;
+                    }
+                }
+                close(fd);
+            }
+            if (found) {
+                
                 remove(file_entry->d_name);
                 char aux[300] = "";
                 sprintf(aux, "Removed %s from %s\n", treasure_id, hunt_entry->d_name);
